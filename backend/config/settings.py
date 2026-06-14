cb007b75fa3f18260deff2a8e43c25ba7d2edc50
Krish Garg <krish@Krishs-MacBook-Air-4.local>
2026-06-14 15:06:17 +0530

    chore: allow production hosts and cors

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -125,8 +125,10 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-    'http://127.0.0.1:3000',
-    'http://192.168.1.120:3000',
-]
+CORS_ALLOW_ALL_ORIGINS = True
+
+# CORS_ALLOWED_ORIGINS = [
+#     'http://localhost:3000',
+#     'http://127.0.0.1:3000',
+#     'http://192.168.1.120:3000',
+# ]
